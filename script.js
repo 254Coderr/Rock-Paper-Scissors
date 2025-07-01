435b297ee228ca22be4819eacffce4675335cf74
Julius Magomere <107256128+254Coderr@users.noreply.github.com>
2025-07-01 12:41:36 +0300

    adding input handling functions on what the user will be able to put

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let computerScore = 0;
 const initGame = () => {
   const startGame = confirm("Shall we play rock, paper, or scissors?");
   startGame ? playGame() : alert("Ok, maybe next time.");
-};/ Play exactly 5 rounds
+};// Play exactly 5 rounds
 const playGame = () => {
 for (let round = 1; round <= 5; round++) {
 alert(`Round ${round} of 5`);
@@ -40,5 +40,36 @@ playRound(playerChoice, computerChoice); // NEW function with scoring logic
 displayFinalResults();
 thanksForPlaying();
 };
+
+
+// Prompt user input
+const getPlayerChoice = () => {
+    return prompt("Please enter rock, paper, or scissors.");
+    };
+    
+    // Format user input
+    const formatPlayerChoice = (playerChoice) => {
+    if (playerChoice || playerChoice === "") {
+    return playerChoice.trim().toLowerCase();
+    } else {
+    return false;
+    }
+    };
+    
+    // Handle if user cancels the game
+    const decidedNotToPlay = () => {
+    alert("I guess you changed your mind. Maybe next time.");
+    };
+    
+    // Validate choice
+    const evaluatePlayerChoice = (playerChoice) => {
+    const validChoices = ["rock", "paper", "scissors"];
+    return validChoices.includes(playerChoice) ? playerChoice : false;
+    };
+    
+    // Inform user of bad input
+    const invalidChoice = () => {
+    alert("Invalid input. Please enter rock, paper, or scissors.");
+    };
 
 
